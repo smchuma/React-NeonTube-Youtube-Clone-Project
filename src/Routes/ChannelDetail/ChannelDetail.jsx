97b048295d7d3a28e76af144c132be8b79a382c5
Samora Laurent <81111079+El-Samorito@users.noreply.github.com>
2022-09-29 18:15:22 +0300

    working on channelCard

--- src/Routes/ChannelDetail/ChannelDetail.jsx
+++ src/Routes/ChannelDetail/ChannelDetail.jsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Videos, ChannelCard } from "../";
+import { fetchApi } from "../../utils/FetchApi";
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+
+  useEffect(() => {
+    fetchApi(`channels?part="snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+  }, [id]);
+
+  console.log(channelDetail);
   return <div>ChannelDetail</div>;
 };
 
